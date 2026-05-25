9b2e49e33d462954c1e9a76d38b58010aab9b265
mor talla seck <mor-talla.SECK@etu.univ-amu.fr>
2026-05-25 21:01:18 +0200

    suppression des lignes de codes inutiles

--- backend/src/main/java/com/terangalink/backend/repository/UserRepository.java
+++ backend/src/main/java/com/terangalink/backend/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.terangalink.backend.repository;
 import com.terangalink.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepository  extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
