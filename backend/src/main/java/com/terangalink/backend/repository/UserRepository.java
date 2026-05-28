7cf324181f67e8ef69087a187e0f5c2ab622acaa
mor talla seck <mor-talla.SECK@etu.univ-amu.fr>
2026-05-28 17:01:31 +0200

    refactorisation complète du module utilisateur et renforcement de l’API

--- backend/src/main/java/com/terangalink/backend/repository/UserRepository.java
+++ backend/src/main/java/com/terangalink/backend/repository/UserRepository.java
@@ -1,8 +1,15 @@
+
 package com.terangalink.backend.repository;
 
 import com.terangalink.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
 }
