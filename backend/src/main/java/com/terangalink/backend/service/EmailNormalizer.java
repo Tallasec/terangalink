23976a862214c963d2cf949ada4fbba2c9cdd453
mor talla seck <mor-talla.SECK@etu.univ-amu.fr>
2026-06-09 14:33:17 +0200

    commentaire de certaines lignes de codes pour une meilleure comprehension

--- backend/src/main/java/com/terangalink/backend/service/EmailNormalizer.java
+++ backend/src/main/java/com/terangalink/backend/service/EmailNormalizer.java
@@ -6,12 +6,12 @@ import java.util.Locale;
 
 @Component
 public class EmailNormalizer {
-
+    // méthodes pour gérer les espacements qu'il peut y avoir dans les emails
     public String normalize(String email) {
         if (email == null) {
             return null;
         }
-        String trimmed = email.trim();
+        String trimmed = email.trim(); // supprime les espacements
         if (trimmed.isEmpty()) {
             return trimmed;
         }
